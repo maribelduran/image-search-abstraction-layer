bfc375fe26682cc1b5ab186c2a5024e0c08ff865
Maribel Duran <maribelduranucm@gmail.com>
2018-02-16 07:31:22 -0800

    Add module to help send google image search requests

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,30 +1,46 @@
 const router = require('express').Router();
 const History = require('../models/historyModel.js')
+const imageSearch = require('node-google-image-search');
 
 //Route to process GET api/imagesearch/:search requests
-  //2) this route should search send the search request to the Google or Bings API server and the respond back with the search results
-    //need an node module that will send requests to either Google search or Bings search API 
 router.route('/imagesearch/:searchTerm')
 .get((req, res) => {
   const searchTerm = req.params.searchTerm;
+  const offset = (req.query.offset) ? req.query.offset : 0;
+  
   //save the search string into the history database
   const history = new History({term: searchTerm});
-  console.log(req.params.searchTerm);
   history.save(function(err, history) {
       if (err) throw err;
       console.log('Saved ' + history);
   });
-  let response = { response: `You want to search for images of ${req.params.searchTerm}`}
-  res.json(response);
-});
   
+  //sends an image search request to Google Custom Search Engine and send the response as an JSON object.
+  const results = imageSearch(searchTerm, updateResults, offset, 2);
+ 
+  function updateResults(images) {
+    if(images.error && images.error.errors) {
+      return images.error;
+    }
+    const imageResults = images.map((image)=>{
+      return  {
+        url: image.link,
+        alt_text: image.snippet,
+        thumbnailLink: image.image.thumbnailLink,
+        contextLink: image.image.contextLink 
+      }
+    });
+    res.json(imageResults);
+ }
+});
+
 //route to process GET api/recent requests
-//this route should send a request to the database and find the top 10 most recent searches and respond back with the results 
 router.route('/recent')
 .get((req, res) => { 
   const query = {};
   const filter = { _id: 0, term: 1, createdAt: 1 };
   
+  //Find the top 10 most recent searches and respond back with the results 
   History.find(query, filter).
   limit(10).
   sort({ createdAt: -1 }).
