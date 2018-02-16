5fbefb4ad0a2fb9b3127f9d65b872d58f14bbc1b
Maribel Duran <maribelduranucm@gmail.com>
2018-02-16 07:31:40 -0800

    Remove unecessary route

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,4 @@ const apiRoutes = require('../api/routes');
 // API Routes
 router.use('/api', apiRoutes);
 
-router.route('/new/')
-.get((req, res) => {
-   let response = { response: "Wondering if this shoud be outside of the api folder!"}
-  res.json(response);
-});
-
 module.exports = router;
